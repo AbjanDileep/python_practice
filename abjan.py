0e3042949ebdd76fe3bac8ae28cc9bcf43dd4162
Abjan Dileep <abjandileep1@gmail.com>
2019-02-08 17:08:40 +0530

    Add files via upload
    8/2/2019

---
+++ abjan.py
@@ -0,0 +1,12 @@
+print("How much kg of sugar do you want..??")
+c = int(input())
+
+d = c * 50
+
+print("\nSugar purchased: ",c,"kg is of value Rs.", d,"\n")
+
+print(f"Sugar purchased: {c}kg is of value Rs.{d}\n")
+
+print("Sugar purchased: {}kg is of value Rs.{}\n".format(c,d))
+
+
---
+++ kms.py
@@ -0,0 +1,10 @@
+
+
+print("how much miles you run...??")
+e=int(input())
+
+f=e/1.60934
+
+
+print(f"kilometers ranned: {e} converted to miles:{f}\n")
+
