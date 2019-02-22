48ce964c877d20aca1c8cd34e3278be25f776ca6
Abjan Dileep <abjandileep1@gmail.com>
2019-02-22 15:40:18 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,10 @@
+print("how old are you..?")
+a=int(input())
+
+if a > 2 and a < 18:
+	print("2$")
+elif a >= 65:
+	print("5$")
+else:
+	print("10$")
+
---
+++ iff.py
@@ -0,0 +1,9 @@
+print("how old are you..?")
+b=int(input())
+if b > 18 and b < 21 :
+	print ("only entry no drink" )
+elif b<18 :
+	print ("no entry" )
+else:
+	print ("entry+ drink" )	
+
