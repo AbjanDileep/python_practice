fba70ff26d9a6dc282e75d226b9a17f02b4a7c4c
Abjan Dileep <abjandileep1@gmail.com>
2019-03-04 16:29:57 +0530

    Add files via upload

---
+++ multi1.py
@@ -0,0 +1,4 @@
+m=int(input("enter a number"))
+
+for i in range(1,11):
+	print(f"{m} X {i} = {m*i}")
---
+++ multi2.py
@@ -0,0 +1,9 @@
+# m=int(input("enter a number"))
+
+for i in range(1,51):
+	# print(f"{m} X {i} = {m*i}")
+	for j in range(1,11):
+		print(f"{i} X {j} = {j*i}")
+	print("\n")
+	
+
