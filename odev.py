b4a927b9125e79f5b76f59db3fc22456ed670aa2
Abjan Dileep <abjandileep1@gmail.com>
2019-03-14 13:46:57 +0530

    Add files via upload

---
+++ odev.py
@@ -0,0 +1,20 @@
+for i in range(0,21):
+	
+	if i%2 == 1 :	
+		
+		if i==13 or i==4 :
+
+			print(f"{i} is unlucky number")
+
+
+		else :
+			print(f"{i} is odd")
+
+
+	else :
+
+		print(f"{i} is even")
+
+
+	
+print("\n")
